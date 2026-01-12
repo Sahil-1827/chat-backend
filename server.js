8ee005d28381625807432ff2b8d48f51325055d0
Sahil <sga.opash@gmail.com>
2026-01-12 13:04:22 +0530

    set process.env.FRONTEND_URL

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 // Configure options with polling as preferred transport
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"], // Specific origin for credentials
+    origin: process.env.FRONTEND_URL, // Specific origin for credentials
     methods: ["GET", "POST"],
     credentials: true
   },
@@ -21,7 +21,7 @@ const io = new Server(server, {
 });
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"], // Specific origin for credentials
+  origin: process.env.FRONTEND_URL, // Specific origin for credentials
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
